544328c026a63e057f3887caff0389aa91043bb1
Sayat <129245004+qwufy@users.noreply.github.com>
2023-04-14 18:00:31 +0600

    ss

---
+++ myArrayList/src/myList.java
@@ -0,0 +1,15 @@
+import javax.print.attribute.standard.JobKOctets;
+
+public interface myList<T> {
+    int size();
+    boolean contains(Object o);
+    void add(T item);
+    void add(T item, int index);
+    boolean remove(T item);
+    T remove(int index);
+    void clear();
+    T get (int index);
+    int indexOf(Object o);
+    int lastIndex(Object o);
+    void sort();
+}
