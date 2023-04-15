52b7a149736292f55f0bff6b49c577aeba9f29f8
Sayat <129245004+qwufy@users.noreply.github.com>
2023-04-15 22:21:58 +0600

    ss

--- myArrayList/src/myArrayList.java
+++ myArrayList/src/myArrayList.java
@@ -3,6 +3,15 @@ public class myArrayList<T> implements myList<T> {
     private Object[] elements;
     private int size;
 
+
+    public myArrayList(int initialCapacity){
+        if(initialCapacity < 0) {
+            throw new IllegalArgumentException("Initial capacity can't be neagative");
+        }
+        elements = new Object[initialCapacity];
+        size = 0;
+    }
+
     @Override
     public int size() {
         return 0;
@@ -10,32 +19,63 @@ public class myArrayList<T> implements myList<T> {
 
     @Override
     public boolean contains(Object o) {
+        for(int i = 0; i < size; i++){
+            if (elements[i].equals(o)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public void add(T item) {
-
+        add(item, size);
     }
 
     @Override
     public void add(T item, int index) {
-
+        if(index < 0 || index > size ){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        if(size == elements.length){
+            Object[] newElements = new Object[elements.length * 2];
+            System.arraycopy(elements, 0, newElements, 0, size);
+            elements = newElements;
+        }
+        System.arraycopy(elements, index, elements, index+1, size - index);
+        elements[index] = item;
+        size++;
     }
 
     @Override
     public boolean remove(T item) {
+        for(int i = 0; i < size; i++){
+            if(elements[i].equals(item)){
+                remove(i);
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if(index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        T removedItem = (T) elements[index];
+        System.arraycopy(elements, index+1,elements,index, size - index);
+        elements[size - 1] = null;
+        size--;
+        return removedItem;
     }
 
     @Override
     public void clear() {
-
+        for(int i = 0; i < size; i++){
+            elements[i] = null;
+        }
+        size = 0;
     }
 
     @Override
