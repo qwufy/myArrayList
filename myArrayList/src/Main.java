f8e400652fe1ec3bf48a63d5493de68f255e0268
Sayat <129245004+qwufy@users.noreply.github.com>
2023-04-18 13:50:23 +0600

    test the methods of myArrayList

--- myArrayList/src/Main.java
+++ myArrayList/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         myArrayList<Integer> myList = new myArrayList<>();
+
         myList.add(1);
         myList.add(2);
         myList.add(3);
