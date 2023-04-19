6b2516a6fe659a4ffbe61f0a325edd0ef848f936
Sayat <129245004+qwufy@users.noreply.github.com>
2023-04-19 16:10:20 +0600

    ss

--- myArrayList/src/myLinkedList.java
+++ myArrayList/src/myLinkedList.java
@@ -26,17 +26,66 @@ public class myLinkedList<T> implements myList<T> {
 
     @Override
     public boolean contains(Object o) {
+        Node currentNode = head;
+        while (currentNode != null){
+            if(currentNode.element.equals(o)){
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public void add(T item) {
-
+        Node newNode = new Node(item);
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail.next = newNode;
+            newNode.previous = tail;
+            tail = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(T item, int index) {
-
+        if(index < 0 || index > size){
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        if(index == size){
+            add(item);
+        }
+        else {
+            Node newNode = new Node(item);
+            if(index == 0){
+                newNode.next = head;
+                head.previous = newNode;
+                head = newNode;
+            }
+            else{
+                Node currentNode;
+                if(index <= size / 2){
+                    currentNode = head;
+                    for (int i = 0; i < index; i++){
+                        currentNode = currentNode.next;
+                    }
+                }
+                else{
+                    currentNode = tail;
+                    for(int i = size - 1; i > index; i--){
+                        currentNode = currentNode.previous;
+                    }
+                }
+                newNode.previous = currentNode.previous;
+                newNode.next = currentNode.next;
+                currentNode.previous.next = newNode;
+                currentNode.previous = newNode;
+            }
+            size++;
+        }
     }
 
     @Override
