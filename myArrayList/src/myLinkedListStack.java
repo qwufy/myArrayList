3b746155ae57528f7f53a911eb5e2505417c1a2b
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-01 22:37:54 +0600

    as3

--- myArrayList/src/myLinkedListStack.java
+++ myArrayList/src/myLinkedListStack.java
@@ -1,26 +1,36 @@
+import java.util.LinkedList;
 import java.util.EmptyStackException;
 
-
 public class myLinkedListStack {
-    private myLinkedList list;
+    private LinkedList<Integer> list;
 
-    public myLinkedListStack(){
-        list = new myLinkedList();
+    public myLinkedListStack() {
+        list = new LinkedList<>();
     }
 
-    public void push(int value){
+    public void push(int value) {
         list.addFirst(value);
     }
 
-    public int pop(){
-        if(list.isEmpty()){
+    public int pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.removeFirst();
+    }
+
+    public int peek() {
+        if (isEmpty()) {
             throw new EmptyStackException();
         }
         return list.getFirst();
     }
 
-    public int size(){
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public int size() {
         return list.size();
     }
 }
-
