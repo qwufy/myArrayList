b4f4c0a4313951aefd7a8ad38baca2ede2d85943
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-01 23:11:16 +0600

    as3

--- myArrayList/src/myArrayListQueue.java
+++ myArrayList/src/myArrayListQueue.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class myArrayListQueue<T> {
     private ArrayList<T> list;
@@ -9,5 +10,19 @@ public class myArrayListQueue<T> {
 
     public void enqueue(T value){
         list.add(value);
+    }
+
+    public T dequeue(){
+        if(list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return list.remove(0);
+    }
+
+    public T peek(){
+        if(list.isEmpty()){
+            throw new NoSuchElementException();
+        }
+        return list.get(0);
     }
 }
