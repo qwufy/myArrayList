334291db837b2b12c2f39c5745a33f6cf377fafc
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-01 22:09:40 +0600

    defense2

--- myArrayList/src/myLinkedListStack.java
+++ myArrayList/src/myLinkedListStack.java
@@ -2,5 +2,20 @@ import java.util.EmptyStackException;
 import myArrayList.MyArrayList; // if implementing MyArrayListStack
 import myLinkedList.MyLinkedList; // if implementing MyLinkedListStack
 public class myLinkedListStack {
-    private myLinkedList list
+    private myLinkedList list;
+
+    public myLinkedListStack(){
+        list = new myLinkedList();
+    }
+
+    public void push(int value){
+        list.addFirst(value);
+    }
+
+    public int pop(){
+        if(list.isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.getFirst();
+    }
 }
