3f2fb4254cf48e9b9233a1de3eb350a0202941cf
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-01 22:49:40 +0600

    as3

--- myArrayList/src/myArrayListStack.java
+++ myArrayList/src/myArrayListStack.java
@@ -11,5 +11,25 @@ public class myArrayListStack {
         list.add(value);
     }
 
+    public int pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.remove(list.size() - 1);
+    }
 
+    public int peek(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        return list.get(list.size() - 1);
+    }
+
+    public boolean isEmpty(){
+        return list.size() == 0;
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
