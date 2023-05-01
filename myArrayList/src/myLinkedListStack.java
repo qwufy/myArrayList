d276870ac1a6e4b7914d93829de5001decb8d20b
Sayat <129245004+qwufy@users.noreply.github.com>
2023-05-01 22:07:36 +0600

    defense2

--- myArrayList/src/myLinkedListStack.java
+++ myArrayList/src/myLinkedListStack.java
@@ -1,47 +1,6 @@
 import java.util.EmptyStackException;
-import java.util.Iterator;
-
-public class myLinkedListStack<T> implements StackList<T>, Iterable<T> {
-    private myLinkedList<T> linkedList;
-
-    public myLinkedListStack() {
-        linkedList = new linkedList<>();
-    }
-
-
-    @Override
-    public void push(T element){
-        linkedList.add(element,0);
-    }
-
-    @Override
-    public T pop(){
-        if(isEmpty()) {
-            throw new EmptyStackException();
-        }
-        return linkedList.remove(0);
-    }
-
-    @Override
-    public T peek(){
-        if(isEmpty()){
-            throw new EmptyStackException();
-        }
-        return linkedList.get(0);
-    }
-
-    @Override
-    public boolean isEmpty(){
-        return linkedList.isEmpty();
-    }
-
-    @Override
-    public int size(){
-        return linkedList.size();
-    }
-
-    @Override
-    public Iterator<T> iterator(){
-        return myLinkedList.iterator();
-    }
+import myArrayList.MyArrayList; // if implementing MyArrayListStack
+import myLinkedList.MyLinkedList; // if implementing MyLinkedListStack
+public class myLinkedListStack {
+    private myLinkedList list
 }
